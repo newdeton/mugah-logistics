6187b0cfc21b8d7a68e20b4d154e106abcdae54c
JNIMULTIPURPOSE <jnimultipurpose001@gmail.com>
2026-06-30 17:44:47 -0500

    Complete booking system, reviews and deployment updates

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,30 @@ function changeImage(image) {
         mainImage.src = image;
     }
 }
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+if (menuToggle && navLinks) {
+
+    menuToggle.addEventListener("click", () => {
+
+        navLinks.classList.toggle("active");
+
+        const icon = menuToggle.querySelector("i");
+
+        if (navLinks.classList.contains("active")) {
+
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-times");
+
+        } else {
+
+            icon.classList.remove("fa-times");
+            icon.classList.add("fa-bars");
+
+        }
+
+    });
+
+}
